7b888ed405b56129f95a36f4bd7930ef5981523e
Mikko Tiainen <mikko.masala@gmail.com>
2018-04-30 23:28:03 +0300

    basic main menu screens

--- grip_box/grip_box.ino
+++ grip_box/grip_box.ino
@@ -6,9 +6,9 @@
 LiquidCrystal_I2C lcd(0x27,16,2);  // set the LCD address to 0x27 for a 16 chars and 2 line display
 
 // UI control button pins
-const int backBtn = A1;
-const int nextBtn = A2;
-const int okBtn = A3;
+const int backButton = A1;
+const int nextButton = A2;
+const int okButton = A3;
 
 // LPT trigger pins
 const int trig1 = 2;
@@ -34,14 +34,16 @@ const int grip2 = A7;
 
 // additionally SPI communication uses pins 11, 12 and 13
 
+int mainScreen = 1;
+
+
 void setup() {
   lcd.init();
   lcd.backlight();
-  lcd.setCursor(0,0);
-  lcd.print("BOOTING UP");
-  pinMode(backBtn, INPUT);
-  pinMode(nextBtn, INPUT);
-  pinMode(okBtn, INPUT);
+  screenPrint("BOOTING UP", "");
+  pinMode(backButton, INPUT);
+  pinMode(nextButton, INPUT);
+  pinMode(okButton, INPUT);
   pinMode(trig1, INPUT);
   pinMode(trig2, INPUT);
   pinMode(trig3, INPUT);
@@ -57,7 +59,56 @@ void setup() {
 }
 
 void loop() {
-  
-  String msg = ((String)"Button 1: " + digitalRead(button1) + ", Button 2: " + digitalRead(button2) + ", Button 3: " + digitalRead(button3));
-  Serial.println(msg);
+  switch (mainScreen) {
+    case 1:
+      screenPrint("READY", "");
+      // display title and some info
+      break;
+    case 2:
+      screenPrint("Test grips", "");
+      // show live recordings from both sensors
+      break;
+    case 3:
+      screenPrint("Test triggers", "");
+      // show live any incoming triggers
+      break;
+    case 4:
+      screenPrint("Change sensitivities", "");
+      // separate menu wizard for changing each pot sensitivity
+      break;
+    case 5:
+      screenPrint("Start recording", "");
+      // separate menu to start recording, ask filename at least
+      break;
+    case 6:
+      screenPrint("log to csv", "");
+      // separate menu to transform a log to csv file, at least file selection
+      break;
+    default:
+      screenPrint("Is something", "wrong?");
+      // should never print
+      break;  
+  }
+  if (digitalRead(nextButton) == 1) {
+    delay(50);
+    if (digitalRead(nextButton)== 1) {
+      mainScreen++;
+      if (mainScreen > 6) {
+        mainScreen = 1;
+      }
+      // delay to avoid accidental proceeding
+      delay(200);
+    }
+  }
+  delay(40);
 }
+
+void screenPrint(String msg1, String msg2) {
+  lcd.clear();
+  lcd.setCursor(0,0);
+  lcd.print(msg1);
+  lcd.setCursor(0,1);
+  lcd.print(msg2);
+}
+
+
